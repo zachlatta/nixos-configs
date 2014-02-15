96d87aada96e9a71532a6b4a152e6c30fae74909
Zach Latta <zach@zachlatta.com>
2014-02-15 02:55:54 -0500

    Changes.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,10 +1,16 @@
-# RVM
-set PATH $PATH $HOME/.rvm/bin
-
 # Go
 set -x GOROOT $HOME/dev/go
 set -x GOPATH $HOME/go
 set PATH $PATH $HOME/go/bin
+
+# Mac specific config
+if [ (uname) = 'Darwin' ]
+  # Docker
+  set -x DOCKER_HOST tcp://
+
+  # Go App Engine
+  set -x PATH $PATH $HOME/go_appengine
+end
 
 . $HOME/.config/fish/aliases.fish
 . $HOME/.config/fish/solarized.fish
