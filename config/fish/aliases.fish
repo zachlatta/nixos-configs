9c7f90cce0de6157a6fa95c9e78ac40e6564b612
Zach Latta <zach@zachlatta.com>
2015-07-04 19:51:08 -0700

    Remove fish config

--- config/fish/aliases.fish
+++
@@ -1,5 +0,0 @@
-alias ..="cd .."
-alias ...="cd ../.."
-
-alias c="xsel --clipboard --input"
-alias p="xsel --clipboard --output"
--- config/fish/functions/ssh_auth_env.fish
+++
@@ -1,4 +0,0 @@
-function ssh_auth_env
-  echo set -x SSH_AUTH_SOCK /run/user/(id -u $USER)/ssh_auth_sock
-  echo set -x SSH_AGENT_PID (systemctl show --user -p MainPID ssh-agent | sed "s/MainPID=//")
-end
