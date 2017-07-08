e1a21941b4d9c162ce3da831d9359f1598f91680
Zach Latta <zach@zachlatta.com>
2017-07-08 02:14:54 -0700

    Wrap text

--- config/bash/main_linux.bash
+++ config/bash/main_linux.bash
@@ -2,7 +2,8 @@
 #
 # Any Linux specific config goes here.
 
-# Start a ssh-agent session if there isn't already one running and save its output.
+# Start a ssh-agent session if there isn't already one running and save its
+# output.
 #
 # If there is already a session running, load its cached output to set the
 # required environment variables for it to work.
