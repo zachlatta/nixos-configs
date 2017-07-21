12e9cea25c207355f8ea5fb6fa825bb8fd62b06f
Zach Latta <zach@zachlatta.com>
2017-07-20 19:53:06 -0700

    Use ansi-universal instead of ansi-light

--- config/bash/main_linux.bash
+++ config/bash/main_linux.bash
@@ -21,5 +21,5 @@ fi
 source "$DOTFILES/lib/bash-completion/bash_completion"
 
 # Make ls nice and pretty
-eval $(dircolors $DOTFILES/lib/dircolors-solarized/dircolors.ansi-light)
+eval $(dircolors $DOTFILES/lib/dircolors-solarized/dircolors.ansi-universal)
 alias ls='ls -F --color'
