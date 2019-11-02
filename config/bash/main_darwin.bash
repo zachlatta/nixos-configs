ef7098aa8cf8df70d5ba6037f86159a4ad90a18a
Zach Latta <zach@zachlatta.com>
2019-11-02 09:07:25 -0700

    Don't complain about bash in Catalina

--- config/bash/main_darwin.bash
+++ config/bash/main_darwin.bash
@@ -2,6 +2,9 @@
 #
 # Any macOS specific config goes here.
 
+# Silence bash deprecation warning in macOS Catalina
+export BASH_SILENCE_DEPRECATION_WARNING=1
+
 # Load up z, our favorite little helper
 if [ ! -f /usr/local/etc/profile.d/z.sh ]; then
   echo "z not installed! Please install it."
