21a16654d0454a230c5cba47595ed4efe243dd59
Zach Latta <zach@zachlatta.com>
2020-05-06 16:54:26 +0000

    Ignore README.md and .git in inbox

--- lib/inbox-notifier/src/main.rs
+++ lib/inbox-notifier/src/main.rs
@@ -16,22 +16,30 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut printed_heading = false;
 
     for entry in fs::read_dir(args.dir)? {
+        let entry = entry?;
+        let path = entry.path();
+        let metadata = fs::metadata(&path)?;
+
+        let filename = path.file_name()
+            .ok_or("no filename")?
+            .to_str()
+            .ok_or("unable to render filename")?;
+
+        // Ignore README and .git directory
+        if filename == "README.md" || filename == ".git" {
+            continue
+        }
+
+        // Print heading if we haven't already
         if !printed_heading {
             println!("You've got files in your inbox!\n");
 
             printed_heading = true;
         }
 
-        let entry = entry?;
-        let path = entry.path();
-        let metadata = fs::metadata(&path)?;
-
         println!(
             " - {}, created on {}",
-            path.file_name()
-                .ok_or("no filename")?
-                .to_str()
-                .ok_or("unable to render filename")?,
+            filename,
             DateTime::<Local>::from(metadata.created()?).format("%Y-%m-%d")
         );
     }
