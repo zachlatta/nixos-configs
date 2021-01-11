08acd5f40d073bbebb0b399c559a7f870533b696
Zach Latta <zach@zachlatta.com>
2021-01-11 17:58:29 -0500

    Fix broken OBS config

--- common/desktop.nix
+++ common/desktop.nix
@@ -26,14 +26,12 @@
     virt-manager
     spotify
     zoom-us
-  ];
 
-  # obs + v4l2sink for virtual OBS webcam
-  #
-  # need to manually run the following after installing (apparently nixos doesn't support obs hooks or something)
-  #
-  # ln -s `nix-build '<nixpkgs>' -A obs-v4l2sink --no-out-link`/share/obs/obs-plugins/v4l2sink ~/.config/obs-studio/plugins/v4l2sink
-  environment.systemPackages = with pkgs; [
+    # obs + v4l2sink for virtual OBS webcam
+    #
+    # need to manually run the following after installing (apparently nixos doesn't support obs hooks or something)
+    #
+    # ln -s `nix-build '<nixpkgs>' -A obs-v4l2sink --no-out-link`/share/obs/obs-plugins/v4l2sink ~/.config/obs-studio/plugins/v4l2sink
     obs-studio
     obs-v4l2sink
   ];
@@ -41,7 +39,7 @@
   boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
   boot.kernelModules = [ "v4l2loopback" ];
   boot.extraModprobeConfig = ''
-    options v4l2loopback exclusive_caps=1 video_nr=9 card_label="OBS"
+    options v4l2loopback exclusive_caps=1 video_nr=9 card_label="obs"
   '';
 
   # open port for spotify so it can sync with local devices on same network
