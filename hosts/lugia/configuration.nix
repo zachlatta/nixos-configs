a17c372e4823f36c434fcb77139c1f40d25fe828
Zach Latta <zach@zachlatta.com>
2021-01-20 22:59:10 -0500

    Fix indentation

--- hosts/lugia/configuration.nix
+++ hosts/lugia/configuration.nix
@@ -80,17 +80,17 @@
 
   # Try out Sway
   programs.sway = {
-  enable = true;
-  wrapperFeatures.gtk = true; # so that gtk works properly
-  extraPackages = with pkgs; [
-    swaylock
-    swayidle
-    wl-clipboard
-    mako # notification daemon
-    alacritty # Alacritty is the default terminal in the config
-    dmenu # Dmenu is the default in the config but i recommend wofi since its wayland native
-  ];
-};
+    enable = true;
+    wrapperFeatures.gtk = true; # so that gtk works properly
+    extraPackages = with pkgs; [
+      swaylock
+      swayidle
+      wl-clipboard
+      mako # notification daemon
+      alacritty # Alacritty is the default terminal in the config
+      dmenu # Dmenu is the default in the config but i recommend wofi since its wayland native
+    ];
+  };
 
   # Enable virtualization
   virtualisation.libvirtd.enable = true;
