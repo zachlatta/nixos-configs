4ad230db35bf27490bdd757aeca3ba1dc6c30b78
Zach Latta <zach@zachlatta.com>
2021-01-31 14:36:05 -0500

    Begin refactor to use sway_common.nix

---
+++ common/sway_common.nix
@@ -0,0 +1,58 @@
+{ config, pkgs, lib, ... }:
+
+{
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm = {
+    enable = true;
+    wayland = true;
+  };
+
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true; # so that gtk works properly
+    extraPackages = with pkgs; [
+      wl-clipboard # make wl-copy and wl-paste available in session
+      mako # notification daemon
+
+      # theme
+      adwaita-qt
+      gnome3.adwaita-icon-theme
+    ];
+  };
+
+  # For the Sway environment, wanted packages go here.
+  #
+  # This should be considered temporary until the Sway environment is either
+  # refactored into a broader config or killed.
+  environment.systemPackages = with pkgs; [
+    tree
+
+    (chromium.override {
+      commandLineArgs = "--enable-features=UseOzonePlatform --ozone-platform=wayland"; # wayland
+    })
+  ];
+
+  # FONTS
+  fonts.fonts = with pkgs; [
+    corefonts
+
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    noto-fonts-emoji-blob-bin
+
+    dejavu_fonts
+    roboto
+    source-sans-pro
+
+    nerdfonts
+
+    liberation_ttf
+    fira-code
+    fira-code-symbols
+    mplus-outline-fonts
+    dina-font
+
+    proggyfonts
+  ];
+}
--- sway-experiment-lugia/default.nix
+++ sway-experiment-lugia/default.nix
@@ -1,62 +1,11 @@
 { config, pkgs, lib, ... }:
 
 {
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm = {
-    enable = true;
-    wayland = true;
-  };
+  imports = [
+    ../common/sway_common.nix
 
-  programs.sway = {
-    enable = true;
-    wrapperFeatures.gtk = true; # so that gtk works properly
-    extraPackages = with pkgs; [
-      wl-clipboard # make wl-copy and wl-paste available in session
-      mako # notification daemon
-
-      # theme
-      adwaita-qt
-      gnome3.adwaita-icon-theme
-    ];
-  };
-
-  # For the Sway environment, wanted packages go here.
-  #
-  # This should be considered temporary until the Sway environment is either
-  # refactored into a broader config or killed.
-  environment.systemPackages = with pkgs; [
-    tree
-
-    (chromium.override {
-      commandLineArgs = "--enable-features=UseOzonePlatform --ozone-platform=wayland"; # wayland
-    })
+    <home-manager/nixos>
   ];
-
-  # FONTS
-  fonts.fonts = with pkgs; [
-    corefonts
-
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    noto-fonts-emoji-blob-bin
-
-    dejavu_fonts
-    roboto
-    source-sans-pro
-
-    nerdfonts
-
-    liberation_ttf
-    fira-code
-    fira-code-symbols
-    mplus-outline-fonts
-    dina-font
-
-    proggyfonts
-  ];
-
-  imports = [ <home-manager/nixos> ];
 
   home-manager.users.zrl = { pkgs, config, ... }: {
     home.packages = [ ];
