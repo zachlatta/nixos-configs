cb5c660e21c789931fed166ea1c6d3e085a1abda
Zach Latta <zach@zachlatta.com>
2021-01-31 14:38:06 -0500

    Refactor psyduck to use sway_common.nix

--- sway-experiment-psyduck/default.nix
+++ sway-experiment-psyduck/default.nix
@@ -1,68 +1,11 @@
 { config, pkgs, lib, ... }:
 
 {
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm = {
-    enable = true;
-    wayland = true;
-  };
+  imports = [
+    ../common/sway_common.nix
 
-  services.gnome3.gnome-keyring.enable = true;
-
-  programs.sway = {
-    enable = true;
-    wrapperFeatures.gtk = true; # so that gtk works properly
-    extraPackages = with pkgs; [
-      wl-clipboard # make wl-copy and wl-paste available in session
-      mako # notification daemon
-
-      # theme
-      adwaita-qt
-      gnome3.adwaita-icon-theme
-
-      # networking
-      gnome3.networkmanagerapplet
-    ];
-  };
-
-  # For the Sway environment, wanted packages go here.
-  #
-  # This should be considered temporary until the Sway environment is either
-  # refactored into a broader config or killed.
-  environment.systemPackages = with pkgs; [
-    git
-    tree
-
-    (chromium.override {
-      commandLineArgs = "--enable-features=UseOzonePlatform --ozone-platform=wayland --disable-gpu-memory-buffer-video-frames"; # wayland
-    })
+    <home-manager/nixos>
   ];
-
-  # FONTS
-  fonts.fonts = with pkgs; [
-    corefonts
-
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    noto-fonts-emoji-blob-bin
-
-    dejavu_fonts
-    roboto
-    source-sans-pro
-
-    nerdfonts
-
-    liberation_ttf
-    fira-code
-    fira-code-symbols
-    mplus-outline-fonts
-    dina-font
-
-    proggyfonts
-  ];
-
-  imports = [ <home-manager/nixos> ];
 
   home-manager.users.zrl = { pkgs, config, ... }: {
     home.packages = [ ];
