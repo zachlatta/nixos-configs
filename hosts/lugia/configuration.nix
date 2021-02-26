29499beddaed20f321c440a5e0eb2bece3949b2a
Zach Latta <zach@zachlatta.com>
2021-02-26 14:49:13 -0500

    Add Abra as authorized NFS client

--- hosts/lugia/configuration.nix
+++ hosts/lugia/configuration.nix
@@ -99,7 +99,7 @@
   # NFS share of home directory
   services.nfs.server.enable = true;
   services.nfs.server.exports = ''
-    /home/zrl   psyduck(rw,fsid=0,no_subtree_check)
+    /home/zrl   psyduck(rw,fsid=0,no_subtree_check) abra(rw,insecure,no_subtree_check,sync,all_squash,anonuid=0,anongid=0)
   '';
 
   # This value determines the NixOS release from which the default
