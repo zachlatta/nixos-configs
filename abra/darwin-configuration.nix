bd75ad9844cb980b431ad4c1ee9a5f9e07b3f73c
Zach Latta <zach@zachlatta.com>
2021-08-10 09:56:01 -0400

    Begin configuration for abra on Darwin platform

---
+++ abra/darwin-configuration.nix
@@ -0,0 +1,38 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ <home-manager/nix-darwin> ];
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages =
+    [
+    ];
+
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  environment.darwinConfig = "/Users/zrl/dev/nixos-configs/abra/darwin-configuration.nix";
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  # Create /etc/bashrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;  # default shell on catalina
+  # programs.fish.enable = true;
+
+  users.users.zrl = {
+    name = "zrl";
+    home = "/Users/zrl";
+  };
+
+  home-manager.users.zrl = { pkgs, ... }: {
+    imports = [
+      ../home-manager/darwin.nix
+    ];
+  };
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+}
