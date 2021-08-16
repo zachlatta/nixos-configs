8f9e4b199abab42d9d104393911d287bc232ab25
Zach Latta <zach@zachlatta.com>
2021-08-16 10:35:01 -0400

    Move home-manager config up

--- lugia/configuration.nix
+++ lugia/configuration.nix
@@ -143,14 +143,6 @@
     };
   };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "20.09"; # Did you read the comment?
-
   home-manager.users.zrl = { pkgs, config, ... }: {
     home.packages = [ ];
 
@@ -188,4 +180,12 @@
       };
     };
   };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.09"; # Did you read the comment?
 }
