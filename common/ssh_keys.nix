e2a16c23c900c7eab3113cb46363c5eb3e8c30bb
Zach Latta <zach@zachlatta.com>
2021-09-12 20:06:42 -0400

    Add abra SSH key

--- common/ssh_keys.nix
+++ common/ssh_keys.nix
@@ -1,4 +1,6 @@
 {
+  abra = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIDO1VSoHnkHOK3ZCqqCRCxtzVtEFast1EWKgFne3tnN zrl@abra
+";
   psyduck = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICSLDfNoAhyP89I99ZgepL6LiZE2jK6A4cqGR4CNceUb zrl@psyduck";
   lugia = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEwQfzs4sQYgvcVKlWFbBKPmFH1yPFBGi4lJMrIlYm/5 zrl@lugia";
 }
