3690e207bc198e77f7f1a7ee3e901d61c011cd7a
Zach Latta <zach@zachlatta.com>
2021-09-16 12:03:40 -0400

    Switch psyduck to use smb... way more reliable

--- psyduck/configuration.nix
+++ psyduck/configuration.nix
@@ -11,7 +11,7 @@
       /home/zrl/dev/nixos-configs/common/tailscale.nix
       /home/zrl/dev/nixos-configs/common/zrl_user.nix
 
-      /home/zrl/dev/nixos-configs/common/pokedex-nfs.nix
+      /home/zrl/dev/nixos-configs/common/pokedex-smb.nix
 
       /home/zrl/dev/nixos-configs/psyduck/sway
     ];
