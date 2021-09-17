3413bc14c3f18e3070e19416da6ecb4f40e68eff
Zach Latta <zach@zachlatta.com>
2021-09-16 23:59:42 -0400

    Add more links to shortlinks, create index page

--- common/shortlinks.nix
+++ common/shortlinks.nix
@@ -1,13 +1,38 @@
 # Set up commonly used URLs as hostnames that nginx redirects to full URLs.
-{ ... }:
+{ lib, pkgs, ... }:
+with builtins;
 let
   links = [
     [ "pokedex" "https://drive.google.com/drive/folders/0ACzwx42JIgErUk9PVA" ]
     [ "notes" "https://app.standardnotes.org/" ]
     [ "slack" "https://hackclub.slack.com/" ]
+    [ "hm-config" "https://rycee.gitlab.io/home-manager/options.html" ]
+    [ "nixos-config" "https://search.nixos.org/options" ]
   ];
+
+  shortlinksIndexName = "shortlinks";
+
+  shortlinksIndex = pkgs.writeTextDir "index.html" ''
+<!DOCTYPE html>
+<html>
+  <head>
+    <style>
+      /* keep links original color after clicking */
+      a:visited {
+        color: blue;
+      }
+    </style>
+  </head>
+  <body>
+    <p>shortlinks</p>
+
+    <ul>
+      ${concatStringsSep "\n" (map (link: "<li><a href=\"http://${elemAt link 0}/\">${elemAt link 0}</a> - ${elemAt link 1}</li>") links) }
+    </ul>
+  </body>
+</html>
+    '';
 in
-with builtins;
 {
   services.nginx.enable = true;
 
@@ -23,20 +48,25 @@ with builtins;
   #   };
   #   ...and so on, for each element
   # };
-  services.nginx.virtualHosts = listToAttrs (map (link:
-    {
-      name = "${elemAt link 0}";
-      value = {
-        locations = {
-          "/" = {
-            return = "302 ${elemAt link 1}";
+  services.nginx.virtualHosts =
+    listToAttrs (map (link:
+      {
+        name = "${elemAt link 0}";
+        value = {
+          locations = {
+            "/" = {
+              return = "302 ${elemAt link 1}";
+            };
           };
         };
+      }
+    ) links) //
+    {
+      "${shortlinksIndexName}" = {
+        root = "${shortlinksIndex}";
       };
-    }
-  ) links);
+    };
 
-
-  # this create an entry like `127.0.0.1 pokedex` for each item in links
-  networking.extraHosts = concatStringsSep "\n" (map (link: "127.0.0.1 ${elemAt link 0}") links);
+  # this creates an entry like `127.0.0.1 pokedex` for each item in links, and it also creates one for the shortlinks index
+  networking.extraHosts = concatStringsSep "\n" (map (link: "127.0.0.1 ${elemAt link 0}") (links ++ [ [ shortlinksIndexName ] ]));
 }
