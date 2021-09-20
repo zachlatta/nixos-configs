2f58a76c761b04ef83f5263bc90afecebad32908
Zach Latta <zach@zachlatta.com>
2021-09-20 10:03:54 -0400

    Use shortlinks on Lugia

--- lugia/configuration.nix
+++ lugia/configuration.nix
@@ -17,6 +17,7 @@
       /home/zrl/dev/nixos-configs/common/zrl_user.nix
 
       /home/zrl/dev/nixos-configs/common/pokedex-smb.nix
+      /home/zrl/dev/nixos-configs/common/shortlinks.nix
 
       #/home/zrl/dev/nixos-configs/common/plasma5.nix
       ./sway
