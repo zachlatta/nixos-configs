54c13f75213c25ce6688be97016726c2ab5b1def
Zach Latta <zach@zachlatta.com>
2021-09-27 18:30:47 -0400

    Run `dhall format` automatically on save

--- home-manager/vim.nix
+++ home-manager/vim.nix
@@ -2,6 +2,8 @@
 
 let
   plugins = pkgs.vimPlugins // pkgs.callPackage ./vim/custom-plugins.nix {};
+
+  dhall = pkgs.dhall;
 in
 {
   programs.vim = {
@@ -33,35 +35,42 @@ in
       number = true;
     };
 
-    extraConfig = ''
-      " theme config
-      let base16colorspace=256
-      colorscheme base16-tomorrow-night
+    extraConfig =
+''
+" theme config
+let base16colorspace=256
+colorscheme base16-tomorrow-night
 
-      " only visually wrap lines on whitespace (https://stackoverflow.com/a/19624717)
-      set nolist wrap linebreak breakat&vim
+" only visually wrap lines on whitespace (https://stackoverflow.com/a/19624717)
+set nolist wrap linebreak breakat&vim
 
-      " CtrlP - no caching
-      let g:ctrl_use_caching = 0
+" CtrlP - no caching
+let g:ctrl_use_caching = 0
 
-      " CtrlP - Ignore files in .gitignore
-      let g:ctrlp_user_command = ['.git', 'cd %s && git ls-files -co --exclude-standard']
+" CtrlP - Ignore files in .gitignore
+let g:ctrlp_user_command = ['.git', 'cd %s && git ls-files -co --exclude-standard']
 
-      " set leader key to SPC
-      nnoremap <SPACE> <Nop> " remove any current binding
-      let mapleader=" "
+" set leader key to SPC
+nnoremap <SPACE> <Nop> " remove any current binding
+let mapleader=" "
 
-      " vim-which-key - Trigger when pressing SPC
-      nnoremap <silent> <leader> :WhichKey '<Space>'<CR>
-      " vim-which-key - Popup after 400ms of no keypress
-      set timeoutlen=400
+" vim-which-key - Trigger when pressing SPC
+nnoremap <silent> <leader> :WhichKey '<Space>'<CR>
+" vim-which-key - Popup after 400ms of no keypress
+set timeoutlen=400
 
-      " leader key keybindings
-      nnoremap <leader>f :NERDTreeFocus<CR>
-      nnoremap <leader>gg :Git<CR>
-      nnoremap <leader>gp :Git push<CR>
-      nnoremap <leader>gf :Git pull<CR>
-      nnoremap <leader>p :CtrlP<CR>
-    '';
+" automatically read files when they've been updated elsewhere
+set autoread
+
+" leader key keybindings
+nnoremap <leader>f :NERDTreeFocus<CR>
+nnoremap <leader>gg :Git<CR>
+nnoremap <leader>gp :Git push<CR>
+nnoremap <leader>gf :Git pull<CR>
+nnoremap <leader>p :CtrlP<CR>
+
+" run dhall format automatically on save
+autocmd bufwritepost *.dhall silent !${dhall}/bin/dhall format %
+'';
   };
 }
