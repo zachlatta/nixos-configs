0bb8f3bcfbd46754e2cc582ab06caea57c5a9da7
Zach Latta <zach@zachlatta.com>
2021-10-24 20:10:44 -0400

    Add comments to Looking Glass config

--- lugia/gpu-passthrough-vm/looking-glass/default.nix
+++ lugia/gpu-passthrough-vm/looking-glass/default.nix
@@ -4,10 +4,12 @@ let
   looking-glass-client-git =
     pkgs.callPackage ../../../pkgs/looking-glass-client-git { };
 in {
+  # set permissions on shared framebuffer so we can access it
   systemd.tmpfiles.rules =
     [ "f /dev/shm/looking-glass 0660 zrl qemu-libvirtd -" ];
 
   environment.systemPackages = [
+    # set up our local installation to have my preferred settings
     (looking-glass-client-git.override {
       commandLineArgs =
         "-f /dev/shm/looking-glass -p 5900 -m 107 -input:mouseSens -9";
