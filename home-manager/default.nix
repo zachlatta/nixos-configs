b871b6bcdb0dbe4f6fe534f80ba5fedc0eaf68c4
Zach Latta <zach@zachlatta.com>
2021-11-13 08:04:10 -0500

    Comment out VS Code server

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,12 +1,12 @@
 { ... }: {
   imports = [
-    ./alacritty.nix
     #./fish.nix
+    #./vscode-server.nix
+    ./alacritty.nix
     ./bash.nix
     ./git.nix
     ./mako.nix
     ./tmux.nix
     ./vim.nix
-    ./vscode-server.nix
   ];
 }
