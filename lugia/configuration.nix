52123e997c8fc8a9c668ed3f5f6f82825c2b58b5
Zach Latta <zach@zachlatta.com>
2021-11-21 14:14:00 -0500

    Use Brother printer driver, no more need for HP drivers

--- lugia/configuration.nix
+++ lugia/configuration.nix
@@ -68,7 +68,7 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
-  services.printing.drivers = [ pkgs.hplip ];
+  services.printing.drivers = with pkgs; [ brlaser ];
 
   # For network discovery of printers
   services.avahi.enable = true;
