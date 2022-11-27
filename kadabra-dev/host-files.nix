bffcb16f1a17af604237b8ef03d4dbaadaef958b
Zach Latta <zach@zachlatta.com>
2022-11-27 04:19:33 +0000

    Switch from NFS to 9p share

--- kadabra-dev/host-files.nix
+++ kadabra-dev/host-files.nix
@@ -1,11 +1,7 @@
 # Make the host computer's user directory available in the VM
 { config, ... }: {
   fileSystems."/mnt/host_user" = {
-    device = "host:/System/Volumes/Data/Users/zrl";
-    fsType = "nfs";
-
-    options = [ "x-systemd.automount" "noauto" ];
+    device = "share";
+    fsType = "9p";
   };
-
-  services.rpcbind.enable = true; # needed for file locking on nfs2 and 3 (i think)
 }
