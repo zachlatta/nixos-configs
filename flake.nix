992ca2b5b83cf0ee38ef8c1407a0a55d1e9443b1
Zach Latta <zach@zachlatta.com>
2025-05-18 23:03:21 -0400

    Add updated Flake

--- flake.nix
+++ flake.nix
@@ -5,9 +5,13 @@
     nixpkgs.url          = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url      = "github:numtide/flake-utils";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, home-manager, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
@@ -26,6 +30,7 @@
         system  = "x86_64-linux";
         modules = [
           ./mew/configuration.nix          # the host config you already have
+          home-manager.nixosModules.home-manager
         ];
         # Pass flake inputs to your modules (optional but handy)
         specialArgs = { inherit (self) inputs; };
